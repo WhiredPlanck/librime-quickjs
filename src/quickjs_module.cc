fe0dc89ad96250cd7ae7b968a82f93dde5cc69e3
WhiredPlanck <whiredplanck@outlook.com>
2024-10-13 18:13:05 +0800

    feat: allow to load script from user and shared data dir

--- src/quickjs_module.cc
+++ src/quickjs_module.cc
@@ -1,5 +1,6 @@
 #include <rime/component.h>
 #include <rime/registry.h>
+#include <rime/service.h>
 #include <rime_api.h>
 
 #include "lib/quickjs.hpp"
@@ -9,12 +10,35 @@
 
 using namespace rime;
 
+namespace fs = std::filesystem;
+
+static void quickjs_initialize(an<qjs::Context> ctx) {
+  JSRegistry::Register("RimeQuickJS", ctx);
+
+  auto &deployer(Service::instance().deployer());
+  const auto userScript = deployer.user_data_dir / "rime.js";
+  const auto sharedScript = deployer.shared_data_dir / "rime.js";
+
+  try {
+    if (fs::exists(userScript)) {
+      LOG(INFO) << "loading user's JavaScript file '" << userScript << "'";
+      ctx->evalFile(userScript.c_str());
+    } else if (fs::exists(sharedScript)) {
+      LOG(INFO) << "loading shared JavaScript file '" << sharedScript << "'";
+      ctx->evalFile(sharedScript.c_str());
+    }
+  } catch (const qjs::exception&) {
+    const auto &e = ctx->getException();
+    LOG(ERROR) << "failed to load rime.js: " << e.as<string>();
+  }
+}
+
 static void rime_quickjs_initialize() {
   LOG(INFO) << "registering components from module 'quickjs'.";
   Registry &r = Registry::instance();
 
   an<QuickJS> qjs(new QuickJS);
-  JSRegistry::Register("RimeQuickJS", qjs->ctx);
+  quickjs_initialize(qjs->ctx);
 
   r.Register("qjs_processor", new QuickJSComponent<QuickJSProcessor>(qjs));
   r.Register("qjs_translator", new QuickJSComponent<QuickJSTranslator>(qjs));
