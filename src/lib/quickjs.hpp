9715fcb5b8a7709f78592b229e8570066edc7e79
WhiredPlanck <whiredplanck@outlook.com>
2026-05-10 13:30:21 +0800

    refactor: remove JS_AddExtraHelpers
    `console` is defined in QuickJS's std helpers.

--- src/lib/quickjs.hpp
+++ src/lib/quickjs.hpp
@@ -3,7 +3,6 @@
 #include <quickjspp.hpp>
 #include "quickjs/quickjs-libc.h"
 #include <rime/common.h>
-#include <string_view>
 
 static JSContext *JS_NewCustomContext(JSRuntime *rt) {
     JSContext *ctx;
@@ -14,22 +13,6 @@ static JSContext *JS_NewCustomContext(JSRuntime *rt) {
     js_init_module_std(ctx, "std");
     js_init_module_os(ctx, "os");
     return ctx;
-}
-
-inline void JS_AddExtraHelper(qjs::Context* ctx) {
-    auto console = ctx->global()["console"];
-    console["info"] = [ctx](std::string_view msg) {
-        LOG(INFO) << msg;
-    };
-    console["warn"] = [](std::string_view msg) {
-        LOG(WARNING) << msg;
-    };
-    console["error"] = [](std::string_view msg) {
-        LOG(ERROR) << msg;
-    };
-    console["debug"] = [](std::string_view msg) {
-        DLOG(INFO) << msg;
-    };
 }
 
 class QuickJS {
